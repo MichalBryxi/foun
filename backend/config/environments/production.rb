13c556964a0e835dfdaffa7801e8c3698c6cfd82
Michal Bryxí <michal.bryxi@gmail.com>
2022-12-01 21:52:51 +0100

    fix: config.active_record.legacy_connection_handling = false

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.active_record.legacy_connection_handling = false
 
   # TODO: 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
