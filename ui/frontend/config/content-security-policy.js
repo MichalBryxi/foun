fdc8f01ecdaa725fd55f780ddbd66b3bfe00bcbb
Michal Bryxí <michal.bryxi@gmail.com>
2022-12-01 23:43:14 +0100

    fix: CSP addon
    - Otherwise JS can't talk to the API :)

---
+++ ui/frontend/config/content-security-policy.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+  return {
+    delivery: ['header'],
+    enabled: true,
+    failTests: true,
+    policy: {
+      'default-src': ["'none'"],
+      'script-src': ["'self'"],
+      'font-src': ["'self'"],
+      'connect-src': ["'self'", 'https://foun-api.pudr.com'],
+      'img-src': ["'self'"],
+      'style-src': ["'self'"],
+      'media-src': ["'self'"],
+    },
+    reportOnly: true,
+  };
+};
