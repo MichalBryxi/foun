195673429875fd88bfe55fa97fe2ae040af84c3c
Michal Bryxí <michal.bryxi@gmail.com>
2022-12-02 01:17:14 +0100

    fix: silence local dev env

--- ui/frontend/config/content-security-policy.js
+++ ui/frontend/config/content-security-policy.js
@@ -5,11 +5,11 @@ module.exports = function () {
     failTests: true,
     policy: {
       'default-src': ["'none'"],
-      'script-src': ["'self'"],
+      'script-src': ["'self'", "'unsafe-inline"],
       'font-src': ["'self'"],
       'connect-src': ["'self'", 'https://foun-api.pudr.com'],
       'img-src': ["'self'"],
-      'style-src': ["'self'"],
+      'style-src': ["'self'", "'unsafe-inline'"],
       'media-src': ["'self'"],
     },
     reportOnly: true,
