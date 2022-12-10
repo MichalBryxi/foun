55ce5be8e22c207fa691db64deefbed4151a7727
Michal Bryxí <michal.bryxi@gmail.com>
2022-12-10 11:29:56 +0100

    fix: proper invoice validation

--- ui/frontend/app/authenticated/import/controller.js
+++ ui/frontend/app/authenticated/import/controller.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import InvoiceValidations from 'foun/validations/invoice';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import PapaParse from 'papaparse';
 import { inject as service } from '@ember/service';
