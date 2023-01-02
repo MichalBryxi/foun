6bffca12965add9fccc6d8a669b3afa6327df962
Michal Bryxí <michal.bryxi@gmail.com>
2023-01-02 23:13:55 +0100

    fix: nice indicators

--- ui/frontend/app/components/data-grid/cell/depth/index.js
+++ ui/frontend/app/components/data-grid/cell/depth/index.js
@@ -1,8 +1,22 @@
 import Component from '@glimmer/component';
 
+const COLORS = {
+  0: 'text-teal-500',
+  1: 'text-sky-500',
+  2: 'text-fuchsia-500',
+  3: 'text-amber-500',
+};
+
 export default class DataGridCellDepthComponent extends Component {
+  get depth() {
+    return this.args.row.cellValue ?? 0;
+  }
+
   get indentation() {
-    if (!this.args.row.cellValue) return [];
-    return [...Array(this.args.row.cellValue).keys()];
+    return [...Array(this.depth).keys()];
+  }
+
+  get color() {
+    return COLORS[this.depth];
   }
 }
