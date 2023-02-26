c2fdd1d22b42f4d132c6a4041e4dace4d952d26b
Michal Bryxí <michal.bryxi@gmail.com>
2023-02-26 17:18:24 +0100

    fix: nicer pull script

--- bin/database_pull.sh
+++ bin/database_pull.sh
@@ -3,9 +3,13 @@
 DUMP_FILENAME='./foun.pudr.com_dump.sql'
 
 source ./backend/.env.production || exit $?
+echo '💾 Creating production dump'
 pg_dump -U $PGUSER -h $PGHOST -p $PGPORT -W -F t $PGDATABASE > $DUMP_FILENAME
 
 source ./backend/.env.development || exit $?
+echo '🎡 Loading dump on localhost'
 dropdb $PGDATABASE -p $PGPORT -U $PGUSER -h $PGHOST
 createdb $PGDATABASE -p $PGPORT -U $PGUSER -h $PGHOST
 pg_restore -U $PGUSER -h $PGHOST -p $PGPORT -W -F t -d $PGDATABASE $DUMP_FILENAME
+
+echo '✨ Success!'
